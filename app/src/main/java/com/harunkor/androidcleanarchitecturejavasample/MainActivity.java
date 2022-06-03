98666f44074abe4f3f00e7a937dbf9304149493c
harunkor <harunkor@gmail.com>
2022-06-03 10:44:52 +0300

    bugfixed

--- app/src/main/java/com/harunkor/androidcleanarchitecturejavasample/MainActivity.java
+++ app/src/main/java/com/harunkor/androidcleanarchitecturejavasample/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.harunkor.androidcleanarchitecturejavasample.domain.model.WeatherModel;
 import com.harunkor.androidcleanarchitecturejavasample.presentation.WeatherViewModel;
@@ -16,20 +17,29 @@ import dagger.hilt.android.AndroidEntryPoint;
 public class MainActivity extends AppCompatActivity {
 
     private WeatherViewModel weatherViewModel;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.result);
         weatherViewModel = new ViewModelProvider(this).get(WeatherViewModel.class);
         weatherViewModel.getWeather("istanbul").observe(this, new Observer<WeatherModel>() {
             @Override
             public void onChanged(WeatherModel weatherModel) {
-                Log.v("DGPAYS",weatherModel.getData().get(0).getWeather().getDescription());
-                Log.v("DGPAYS",weatherModel.getData().get(0).getWeather().getIcon());
-                Log.v("DGPAYS",weatherModel.getData().get(0).getWeather().getCode().toString());
-                Log.v("DGPAYS",weatherModel.getData().get(0).getAppTemp().toString());
-                Log.v("DGPAYS",weatherModel.getData().get(0).getDatetime());
+                textView.append(weatherModel.getData().get(0).getWeather().getDescription()+"\n");
+                textView.append(weatherModel.getData().get(0).getWeather().getIcon()+"\n");
+                textView.append(weatherModel.getData().get(0).getWeather().getCode().toString()+"\n");
+                textView.append(weatherModel.getData().get(0).getAppTemp().toString()+"\n");
+                textView.append(weatherModel.getData().get(0).getDatetime()+"\n");
+                textView.append(weatherModel.getData().get(0).getCityName()+"\n");
+                textView.append(weatherModel.getData().get(0).getClouds().toString()+"\n");
+                textView.append(weatherModel.getData().get(0).getStation()+"\n");
+                textView.append(weatherModel.getData().get(0).getSunrise()+"\n");
+                textView.append(weatherModel.getData().get(0).getSunset()+"\n");
+                textView.append(weatherModel.getData().get(0).getClouds()+"\n");
+                textView.append(weatherModel.getData().get(0).getSolarRad()+"\n");
 
             }
         });
